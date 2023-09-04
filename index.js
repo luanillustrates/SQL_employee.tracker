606b78da8db7467debe74ae66467ff8f141b9276
luanillustrates <luanillustrates@gmail.com>
2023-09-04 22:58:26 +0930

    added viewing and adding functions for depts roles

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const sqlConnection = require('./db/sqlConnection');
 const figlet = require('figlet');
 
 // splash title 
@@ -57,25 +58,82 @@ const menuStart = () => {
     });
 };
 
-
-
-// brings back to start of cli
-function start() {
-    inquirer.prompt(cliEnquirer)
-        .then((response) => {
-            menuStart(response.tasks);
-        })
+// department functions
+const viewAllDepts = () => {
+    sqlConnection.query('SELECT * FROM departments', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        menuStart();
+    });
 };
 
-function quit () {
+const addDept = () => {
+    inquirer.prompt({
+        type: 'input',
+        name: 'dept_name',
+        message: 'What is the new department called?',
+    }).then((answer) => {
+        sqlConnection.query('INSERT INTO departments SET ?', { dept_name: answer.dept_name }, (err, res) => {
+            if (err) throw err;
+            console.log('department successfully added');
+            menuStart();
+        });
+    });
+};
+
+const checkDeptId = (deptID, callback) => {
+    const query = 'SELECT * FROM departments WHERE id = ?';
+    sqlConnection.query(query, [deptID], (err, res) => {
+        if (err) throw err;
+        if (res.length === 0) {
+            console.log('invalid ID. Add the department');
+            startMenu();
+        }  else {
+            callback();
+        }
+    })
+}
+
+// role functions
+const viewRole = () => {
+    sqlConnection.query('SELECT * FROM roles', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        menuStart();
+    });
+};
+
+const addRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the new role titled?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary for this role?'
+        },
+        {
+            type: 'input',
+            name: 'department_id',
+            message: 'What is the department ID for this role?'
+        },
+    ]).then((answer) => {
+        checkDeptId(answer.department_id, () => {
+            sqlConnection.query('INSERT INTO roles SET ?', answer, (err, res) => {
+                if (err) throw err;
+                console.log('role successfully added');
+                menuStart();
+            });
+        });
+    });
+};
+
+function quit() {
     console.log('Clocking out');
     process.exit();
 }
 
-function init() {
-    start();
-};
-
-init();
-
-exports.start = start;
+menuStart();
