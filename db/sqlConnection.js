81f51eb746feb5e72ce3a6123c1422cd5c37dfa0
luanillustrates <luanillustrates@gmail.com>
2023-09-04 20:50:57 +0930

    combined cli into one file, moved  sqlconnection

--- db/sqlConnection.js
+++ db/sqlConnection.js
@@ -1,12 +1,10 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection(
-    {
-        database: employee_db,
-        user: root,
+const connection = mysql.createConnection({
+        database: 'employee_db',
+        user: 'root',
         password: process.env.DB_PASSWORD
-    },
-);
+});
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const inquirer = require('inquirer');
-const cliEnquiry = require('./cliQuestions');
 const figlet = require('figlet');
 
 // splash title 
 console.log(
     figlet.textSync("Employee Tracker", {
-      font: "standard",
-      horizontalLayout: "default",
-      verticalLayout: "default",
-      whitespaceBreak: true,
+        font: "standard",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+        whitespaceBreak: true,
     })
 );
 
-// option lists for db tasks
-const cliEnquirer = [
-    {
+// option lists for db tasks cli
+const menuStart = () => {
+    inquirer.prompt({
         type: 'list',
         name: 'tasks',
         message: 'What would you like to do?',
@@ -27,20 +26,56 @@ const cliEnquirer = [
             'Add employee',
             'Update employee role',
             'Quit',
-        ]
-    }
-];
+        ],
+    }).then((answer) => {
+        switch (answer.tasks) {
+            case 'View all departments':
+                viewAllDepts();
+                break;
+            case 'Add department':
+                addDept();
+                break;
+            case 'View all roles':
+                viewRole();
+                break;
+            case 'Add role':
+                addRole();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add employee':
+                addEmployee();
+                break;
+            case 'Update employee role':
+                updateEmployee();
+                break;
+            case 'Quit':
+                quit();
+                break;
+        }
+    });
+};
+
+
 
 // brings back to start of cli
 function start() {
     inquirer.prompt(cliEnquirer)
-    .then((response) => {
-        cliEnquiry(response.tasks);
-    })
+        .then((response) => {
+            menuStart(response.tasks);
+        })
 };
+
+function quit () {
+    console.log('Clocking out');
+    process.exit();
+}
 
 function init() {
     start();
 };
 
 init();
+
+exports.start = start;
